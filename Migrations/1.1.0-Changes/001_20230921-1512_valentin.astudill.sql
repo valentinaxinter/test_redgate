cbe7f863cd038b20a8ca3e1e0096469111ea82ce
Valentin Astudillo <admin.valentin.astudillo@axinter.network>
2023-09-21 15:39:29 +0200

    Delete extra table

---
+++ Migrations/1.1.0-Changes/001_20230921-1512_valentin.astudill.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="7718d208-5e60-4a5b-9d8b-456b633ce956" />
+GO
+
+PRINT N'Dropping constraints from [dbo].[test_to]'
+GO
+ALTER TABLE [dbo].[test_to] DROP CONSTRAINT [PK__test_to__3214EC07ECC99638]
+GO
+PRINT N'Dropping [dbo].[test_to]'
+GO
+DROP TABLE [dbo].[test_to]
+GO
